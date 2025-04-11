0c37eaf138c47a5d1da74a4dfef2eac8ec4c8560
diepmthuan <thuandmps38008@gmail.com>
2025-04-11 23:47:44 +0700

    Fist

---
+++ src/main/java/poly/edu/Calculator.java
@@ -0,0 +1,15 @@
+package poly.edu;
+
+
+public class Calculator implements calculatorInterface {
+
+	@Override
+	public Integer sum(Integer a, Integer b) {
+		return a/b;
+	}
+	@Override
+	public Integer sub(Integer a, Integer b) {
+		return a/2;
+	}
+
+}
---
+++ src/main/java/poly/edu/calculatorInterface.java
@@ -0,0 +1,6 @@
+package poly.edu;
+
+public interface calculatorInterface {
+	Integer sum(Integer a, Integer b);
+	Integer sub(Integer a, Integer b);
+}
