3995dd1a3b10d708f456d36654fd085aef86c5b4
diepmthuan <thuandmps38008@gmail.com>
2025-04-12 00:41:09 +0700

    test đầu tiên

--- src/test/java/poly/edu/TestCalculator.java
+++ src/test/java/poly/edu/TestCalculator.java
@@ -7,11 +7,11 @@ public class TestCalculator {
 	@Test
 	public void testSum() {
 		Calculator ca = new Calculator();
-		Assert.assertEquals(ca.sum(3, 2), 5);
+		Assert.assertEquals(ca.sum(3, 2), 2);
 	}
 	@Test
 	public void testSub() {
 		Calculator ca = new Calculator();
-		Assert.assertEquals(ca.sub(3, 2), 10);
+		Assert.assertEquals(ca.sub(3, 2), 4);
 	}
 }
